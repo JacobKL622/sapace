973eff90fe82fea8709d790ab76f84a938c95faf
JacobKL622 <123975492+JacobKL622@users.noreply.github.com>
2023-02-01 10:32:33 -0700

    Add files via upload

--- turtle_1.py
+++ turtle_1.py
@@ -1,4 +1,24 @@
 import turtle
+bullet_speed = 5
+speed=20
+def move_left():
+    x=player.xcor() -speed
+    if x<-249:
+        x=-249
+    player.setx(x)
+def move_right():
+    x=player.xcor() +speed
+    if x>249:
+        x=249
+    player.setx(x)
+
+def shoot():
+    x=player.xcor()
+    y=player.ycor()
+    bullet.setposition(x,y+40)
+    bullet.showturtle()
+
+
 
 #setting up the screen
 scr1 = turtle.Screen()
@@ -18,8 +38,34 @@ for side in range(4):
     border_pen.lt(90)
 border_pen.hideturtle()
 
+#downloads image to shape database -- copy paste to shape()
+turtle.register_shape("images/rocket2.gif")
+
+player = turtle.Turtle()
+player.shape("images/rocket2.gif")
+player.penup()
+player.speed(0)
+player.setposition(0, -250)
+
+#bullet
+bullet=turtle.Turtle()
+bullet.color("lightblue")
+bullet.shape("triangle")
+bullet.up()
+bullet.speed(0)
+bullet.setheading(90)
+bullet.shapesize(.5,.5)
+bullet.hideturtle()
 
 
 
+#keyboard binds
+turtle.listen()
+turtle.onkey(move_left, "Left")
+turtle.onkey(move_right, "Right")
+turtle.onkey(shoot, "space")
+
+while True:
+    bullet.forward(bullet_speed)
 
 delay = input("Enter")
