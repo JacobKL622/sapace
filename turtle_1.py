08dce6acd712be3f26761f503fd9b31428373ac8
JacobKL622 <123975492+JacobKL622@users.noreply.github.com>
2023-02-07 10:18:46 -0700

    Add files via upload

--- turtle_1.py
+++ turtle_1.py
@@ -1,5 +1,9 @@
 import turtle
-bullet_speed = 5
+import time
+import random
+
+bullet_speed = 9
+invader_speed = 3
 speed=20
 def move_left():
     x=player.xcor() -speed
@@ -41,26 +45,43 @@ border_pen.hideturtle()
 #downloads image to shape database -- copy paste to shape()
 turtle.register_shape("images/rocket2.gif")
 turtle.register_shape("images/rocket3.gif")
+turtle.register_shape("images/rocket4.gif")
 player = turtle.Turtle()
 player.shape("images/rocket2.gif")
 player.penup()
 player.speed(0)
 player.setposition(0, -250)
 
-#bullet
+#bullet 
+#Creates bullet and shape and postion etc
 bullet=turtle.Turtle()
 bullet.color("lightblue")
-bullet.shape("triangle")
+bullet.shape("images/rocket4.gif")
 bullet.up()
 bullet.speed(0)
 bullet.setheading(90)
 bullet.shapesize(.5,.5)
 bullet.hideturtle()
 
-#invader turtle
+#invader turtle 
+#Creates the invader turtle
 invader=turtle.Turtle()
 invader.shape("images/rocket3.gif")
+invader.up()
+invader.speed(0)
+invader.setposition(-260,260)
 
+#score stuff
+score = 0
+
+#creates scoreboard
+score_pen=turtle.Turtle()
+score_pen.speed(0)
+score_pen.color("Yellow")
+score_pen.up()
+score_pen.setposition(-290,310)
+score_pen.write("Score %s" %score)
+score_pen.hideturtle()
 
 #keyboard binds
 turtle.listen()
@@ -68,7 +89,25 @@ turtle.onkey(move_left, "Left")
 turtle.onkey(move_right, "Right")
 turtle.onkey(shoot, "space")
 
+#I have no clue. detects if bullet hits enemy, updates score, and stuff :)
 while True:
     bullet.forward(bullet_speed)
+    invader.forward(invader_speed)
+    if invader.xcor()>260 or invader.xcor()<-260:
+        invader.right(180)
+        invader.forward(invader_speed)
+
+    if abs(bullet.xcor()-invader.xcor())<15 and abs(bullet.ycor()-invader.ycor())<15:
+        bullet.hideturtle()
+        score = score+10
+        score_pen.clear()
+        score_pen.write("Score %s" %score)
+        invader.hideturtle()
+        
+        invader.showturtle()
+        x=random.randint(-250,250)
+        invader.setposition(x,250)
+
+        player.setposition(0,-250)
 
 delay = input("Enter")
